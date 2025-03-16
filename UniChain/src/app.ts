8d11ca05715bd88511e62fbf3fb2ba0295ed2b52
yohhannees <yohannesgetachewerieso@gmail.com>
2025-03-16 20:12:30 +0300

    Updated

--- UniChain/src/app.ts
+++ UniChain/src/app.ts
@@ -12,6 +12,8 @@ import studentDegreeRoutes from "./routes/studentDegree.routes";
 import lostItemRoutes from './routes/lostItem.routes';
 import foundItemRoutes from './routes/foundItem.routes';
 import lostClaimRoutes from './routes/lostClaim.routes';
+import walletRoutes from './routes/wallet.routes';
+import notificationRoutes from './routes/notification.routes';
 
 const app = express();
 
@@ -32,6 +34,9 @@ app.use("/api/student-degrees", studentDegreeRoutes);
 app.use('/api/lostItems', lostItemRoutes);
 app.use('/api/foundItems', foundItemRoutes);
 app.use('/api/claims', lostClaimRoutes);
+
+app.use('/api/wallet', walletRoutes);
+app.use('/api/notifications', notificationRoutes);
 
 app.get("/", (req, res) => {
   res.send("Unichain Service  Is  Running");
